43aa459d9d4eb56dd9165f31a8d9e0d46c2dc202
SyahrenMaulana <sfa.asus@gmail.com>
2022-03-21 11:41:20 +0700

    notification

--- app/src/main/java/www/smktelkommlg/myrecyclerview/GridHeroAdapter.kt
+++ app/src/main/java/www/smktelkommlg/myrecyclerview/GridHeroAdapter.kt
@@ -16,7 +16,6 @@ import com.bumptech.glide.request.RequestOptions
 class GridHeroAdapter(private val listHero: ArrayList<Hero>) : RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
     inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GridViewHolder {
--- app/src/main/java/www/smktelkommlg/myrecyclerview/MainActivity.kt
+++ app/src/main/java/www/smktelkommlg/myrecyclerview/MainActivity.kt
@@ -1,9 +1,19 @@
 package www.smktelkommlg.myrecyclerview
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +25,8 @@ import www.smktelkommlg.myrecyclerview.HeroesData
 class MainActivity : AppCompatActivity() {
     private lateinit var rvHeroes: RecyclerView
     private var list: ArrayList<Hero> = arrayListOf()
+    private var channel_id = "NOTIFY"
+    private var notificationId = 1092
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -70,7 +82,35 @@ class MainActivity : AppCompatActivity() {
                 title = "Mode CardView"
                 showRecyclerCardView()
             }
-        }
 
+            R.id.get_notification -> {
+                title = "Get Notif"
+                startTripNotification()
+            }
+        }
+    }
+
+    private fun initChannel(channelId: String, channelName: String) {
+        if (Build.VERSION.SDK_INT < 26) {
+            return
+        }
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
+
+        notificationManager.createNotificationChannel(channel)
+    }
+    private fun startTripNotification() {
+
+        initChannel(channel_id, channelName = "kotlin")
+
+        val pendingIntent = PendingIntent.getActivity(this, 0, Intent(), 0)
+        val     notification = NotificationCompat.Builder(this,channel_id)
+            .setContentTitle("Notification")
+            .setContentText("Buka lah")
+            .setSmallIcon(R.drawable.logo)
+            .setLargeIcon(BitmapFactory.decodeResource(this.resources, R.drawable.ic_launcher_background))
+        notification.setContentIntent(pendingIntent)
+        val notificationManager = this?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.notify(0, notification.build())
     }
 }
